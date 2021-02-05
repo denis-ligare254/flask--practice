b38b250630c85d33659a937c526cdc31f3e8fab3
denis <mrisholigare@gmail.com>
2021-02-05 13:03:57 +0300

    template folder added,inside,i added a .html file

--- flashcard.py
+++ flashcard.py
@@ -1,10 +1,10 @@
-from flask import  Flask
+from flask import  Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "welcome to flash application"
+    return render_template('welcome.html')
 
 
 
@@ -15,3 +15,4 @@ def welcome():
 # to  run the project
 # 1.export FLASK_APP="NAMEOFPROJECT" it tells flask which module our application can be found
 # 2.export FLASK_ENV=development-To switch Flask to the development environment and enable debug mode.it enables some design features like the debugger
+# select interpreter(to solve "cannot import module flask") by clicking cntrl shift p
